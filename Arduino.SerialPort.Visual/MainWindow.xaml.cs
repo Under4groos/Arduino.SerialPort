096643b90771a1c92d9efad4ea058545480434a5
UnderKo <hhit934@gmail.com>
2024-01-10 02:57:38 +0300

    Пизда просто

--- Arduino.SerialPort.Visual/MainWindow.xaml.cs
+++ Arduino.SerialPort.Visual/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.IO.Ports;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +13,95 @@ using System.Windows.Shapes;
 
 namespace Arduino.SerialPort.Visual
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+ 
     public partial class MainWindow : Window
     {
+        Random r = new Random();
+        System.IO.Ports.SerialPort serialPort = new System.IO.Ports.SerialPort("COM4", 9600);
+        string DATA_ = "";
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+            serialPort.ReadTimeout = 4000;
+            serialPort.WriteTimeout = 4000;
+            serialPort.Encoding = Encoding.UTF8;
+            serialPort.DataReceived += SerialPort_DataReceived;
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ee)
+            {
+
+                MessageBox.Show(ee.Message);
+            }
+            
+        }
+        string[] array_ = { };
+        string[] array_2 = { };
+        Dictionary<string, string> map_ = new Dictionary<string, string>();
+        void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                DATA_ = serialPort.ReadExisting().Replace("\n" , "|");
+
+
+
+                array_ = DATA_.Split("|");
+                foreach (var item in array_)
+                {
+                    array_2 = item.Split(":");
+                    if (array_2.Length == 1)
+                        continue;
+                    if (map_.ContainsKey(array_2[0]))
+                    {
+                        map_[array_2[0]] = array_2[1];
+                    }
+                    else
+                    {
+                        map_.Add(array_2[0], array_2[1]);
+                    }
+                }
+                foreach (var item in map_)
+                {
+                    Debug.WriteLine($"{item.Key}: {item.Value}");
+
+                    switch (item.Key)
+                    {
+
+                        case "4":
+                            if(item.Value == "1")
+                                this.Dispatcher.BeginInvoke(() =>
+                                {
+                                    _border.Background = new SolidColorBrush(Color.FromRgb((byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 233)));
+
+                                });
+
+                            break;
+                        default:
+                            break;
+                    }
+
+
+                }
+
+               
+
+
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
--- Arduino.nano/sketch_jan10a/sketch_jan10a.ino
+++ Arduino.nano/sketch_jan10a/sketch_jan10a.ino
@@ -1,4 +1,5 @@
 
+
 int pin = A4;
 int pin2 = A5;
 void setup() {
@@ -6,16 +7,22 @@ void setup() {
   
   pinMode(pin, INPUT_PULLUP );   
   pinMode(pin2, INPUT_PULLUP );   
-
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 bool button_down = false;
 bool button_down_toggle = false;
 
-void WritePin( String pin , bool data ){
+void WritePin( String pin_ , bool data ){
  
-  Serial.println( pin + ":" + String(data) );
+  Serial.print( pin_ + ":" + String(data) + "\n" );
+  if(data){
+    digitalWrite(LED_BUILTIN, HIGH);
 
+  }else{
+    digitalWrite(LED_BUILTIN, LOW);
+    
+  }
   
 }
 
